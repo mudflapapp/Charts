0363275e9a9d2763bd8a43be192ea5a8ce093ece
joshavant <joshavant@gmail.com>
2023-03-27 15:13:17 -0500

    allow users to not round corner of overlapping bars

--- Source/Charts/Renderers/BarChartRenderer.swift
+++ Source/Charts/Renderers/BarChartRenderer.swift
@@ -54,6 +54,8 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
     // [CGRect] per dataset
     private var _buffers = [Buffer]()
     
+    open var shouldDrawStackedBarWithRoundedCornersOnlyAtTopOfBar: Bool = false
+    
     open override func initBuffers()
     {
         guard let barData = dataProvider?.barData else { return _buffers.removeAll() }
@@ -370,10 +372,27 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
          for j in buffer.indices
          {
              let barRect = buffer[j]
-
+             
+             var adjustedCornerRadius: CGFloat = cornerRadius
+             
+             if shouldDrawStackedBarWithRoundedCornersOnlyAtTopOfBar {
+                 let overlappingBars = buffer.filter({ $0.origin.x == barRect.origin.x })
+                 
+                 if overlappingBars.count > 1 {
+                     if let maxOverlappingBarHeight = overlappingBars.max(by: { $0.height < $1.height }),
+                        barRect.height >= maxOverlappingBarHeight.height {
+                         // barRect does have the largest height value, of all overlapping bars at this x-value
+                         // do nothing
+                     } else {
+                         // barRect does NOT have the largest height value, of all overlapping bars at this x-value
+                         adjustedCornerRadius = 0
+                     }
+                 }
+             }
+             
              let maskPath = UIBezierPath(roundedRect: barRect,
                                          byRoundingCorners: roundedCorners,
-                         cornerRadii: CGSize(width: cornerRadius, height: cornerRadius)
+                         cornerRadii: CGSize(width: adjustedCornerRadius, height: adjustedCornerRadius)
              )
 
              let cgPath = maskPath.cgPath
